4fd0cc3122f158bd1c69095e0b8cf3581421245f
Alex Hughes <alexhughes@AlexMBPro.local>
2019-06-07 13:56:39 -0700

    add build

---
+++ build/precache-manifest.7bac3c4fbd7949164421f725cad8aa54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada2e38b1131000dd07cb6d0602d4411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d50194297b871c74b8ba",
+    "url": "/static/css/main.f8591291.chunk.css"
+  },
+  {
+    "revision": "daa3a4631dae3f344078",
+    "url": "/static/js/2.ee6919d3.chunk.js"
+  },
+  {
+    "revision": "d50194297b871c74b8ba",
+    "url": "/static/js/main.83101fda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a7fea3d39398b71b0242ab5d9b65d41",
+    "url": "/static/media/Apod.1a7fea3d.mp4"
+  },
+  {
+    "revision": "60b9f2b1eca8d13c6657d25cc20118a2",
+    "url": "/static/media/Background1.60b9f2b1.mp4"
+  },
+  {
+    "revision": "ddae2bbc4cd32c053ab9ad4b3a54dd05",
+    "url": "/static/media/Coordinates.ddae2bbc.mp4"
+  },
+  {
+    "revision": "bc44b53f84bf3680c5ce610acfe11585",
+    "url": "/static/media/Earth.bc44b53f.mp4"
+  },
+  {
+    "revision": "9a518260846860e1f05b1676e9f98f93",
+    "url": "/static/media/Notes.9a518260.mp4"
+  },
+  {
+    "revision": "f0c7fdea57bda888984965916419b865",
+    "url": "/static/media/mainPageButton.f0c7fdea.jpg"
+  }
+]);
